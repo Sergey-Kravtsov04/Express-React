9e712bb8e2fd3ce4f40cb299b34123acb4e27176
Sergey-Kravtsov04 <kravtsovsa04@mail.ru>
2025-04-09 20:11:37 +0300

    Validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import express from "express";
+import express, { json } from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
 
 mongoose.connect('mongodb+srv://admin:qqqqqq@cluster0.0vyfzlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('DB ok'))
@@ -14,13 +16,19 @@ app.get('/',(req,res)=>{
     res.send('Hello, world!!!&')
 });
 
-app.post('/auth/login',(req,res)=>{
-    
-})
+app.post('/auth/register',registerValidation,(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors.array());
+    }
+    res.json({
+        result:"success"
+    })
+});
 
 app.listen(4444,(err)=>{
     if(err){
        return console.error(err);
     }
     console.log('Server OK');
-})
+});
---
+++ validations/auth.js
@@ -0,0 +1,9 @@
+import {body} from "express-validator"
+
+export const registerValidation = [
+    body("fullName").isLength({min:3}),
+    body("email").isEmail(),
+    body("password").isLength({min:5}),
+    body("avatarUrl").optional().isURL(),
+    body("major").optional()
+]
